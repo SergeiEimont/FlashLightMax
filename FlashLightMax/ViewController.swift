445807d1f74e40c06c8afbe48e13262167b8ea2b
Sergei Eimont <s.eimont@yandex.ru>
2022-01-31 01:57:58 +0300

    Initial Commit

---
+++ FlashLightMax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashLightMax
+//
+//  Created by Сергей Эймонт on 31.01.2022.
+//  Copyright © 2022 Сергей Эймонт. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
